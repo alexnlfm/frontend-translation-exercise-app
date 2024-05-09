3610e270aaef6596fb6eb5c5f80eea6dae52398a
alexander_shvartskopf <alex.s@progg.io>
2024-05-09 23:48:30 +0300

    Initial commit

---
+++ src/components/DetailsViewWidget/DetailsViewWidget.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ActionsSection from './components/ActionsSection/ActionsSection';
+import DetailsHeader from './components/DetailsHeader/DetailsHeader';
+import GeneralDetailsSection from './components/GeneralDetailsSection/GeneralDetailsSection';
+import {VerticalContainer} from '../../styles/commonStyles';
+import styled from 'styled-components';
+
+const WidgetContainer = styled(VerticalContainer)`
+  min-height: 100%;
+  background-color: white;
+  box-shadow: 0px 1px 4px 1px #888888;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  padding-top: 15px;
+  flex: 1;
+`;
+
+export default function DetailsViewWidget({resource, resourceActions}) {
+    const {name: resourceName} = resource;
+    return (
+        <WidgetContainer>
+            <DetailsHeader {...{resourceName}} />
+            <ContentContainer>
+                <GeneralDetailsSection {...{resource}} />
+                <ActionsSection {...{resourceActions}} />
+            </ContentContainer>
+        </WidgetContainer>
+    );
+}
+
+DetailsViewWidget.propTypes = {
+    resource: PropTypes.object,
+    resourceActions: PropTypes.array
+};
